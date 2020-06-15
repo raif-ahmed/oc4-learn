13c393e4bbf30b98fec9a50424f312255c7e7613
Raif Ahmed <rahmed@redhat.com>
2020-06-15 10:24:57 +0200

    IBM s3

--- ibm-cloud/00_create_cluster.sh
+++ ibm-cloud/00_create_cluster.sh
@@ -1,14 +1,14 @@
 # to get available zones 
 # ibmcloud oc zone ls --provider classic
 export DataCenterZone=ams03
-export ClusterName="myopenshift"
+export ClusterName="democluster"
 
 # Ensure cluster name is lower case and no _,as it makes some issues to s3
 ClusterName=$(sed -e 's/\(.*\)/\L\1/' <<< "$ClusterName")
 echo "ClusterName is ${ClusterName}"
 
 
-ibmcloud login -a https://api.eu-de.bluemix.net -r eu-de -u rahmed@redhat.com -p xxxxx -c 63cf37b8c3bb448cbf9b7507cc8ca57d -g benelux
+ibmcloud login -a https://api.eu-de.bluemix.net -r eu-de -u rahmed@redhat.com -p xxxxxx -c 63cf37b8c3bb448cbf9b7507cc8ca57d -g benelux
 
 export PrivateVlanId=$(ibmcloud sl vlan list -d $DataCenterZone --output json | jq '.[] | select(.networkSpace=="PRIVATE")' | jq ."id"| head -n1)
 echo "private is ${PrivateVlanId}"
@@ -64,13 +64,15 @@ COSServiceId=$(sed -e 's/^"//' -e 's/"$//' <<<"$COSServiceId")
 
 echo "COSServiceId=$COSServiceId"
 # Creating credentials for IBM object storage service
-ibmcloud resource service-key-create "$ClusterName"-creds Writer --instance-name "$ClusterName"-cos
+
+ibmcloud resource service-key-create "$ClusterName"-creds Writer --instance-name "$ClusterName"-cos --parameters '{"HMAC":true}'
 
 export CredServiceKeyId=$(ibmcloud resource service-key "$ClusterName"-creds --output json | jq '.[]'|  jq .'crn')
-export CredAPIKey=$(ibmcloud resource service-key "$ClusterName"-creds  --output json | jq '.[]'|  jq .'credentials.apikey')
+export AccessKeyId=$(ibmcloud resource service-key "$ClusterName"-creds --output json | jq '.[]'|  jq .'credentials.cos_hmac_keys.access_key_id')
+export SecretAccessKey=$(ibmcloud resource service-key "$ClusterName"-creds --output json | jq '.[]'|  jq .'credentials.cos_hmac_keys.secret_access_key')
 
-echo "CredServiceKeyId=$CredServiceKeyId"
-echo "CredAPIKey=$CredAPIKey"
+AccessKeyId=$(sed -e 's/^"//' -e 's/"$//' <<<"$AccessKeyId")
+SecretAccessKey=$(sed -e 's/^"//' -e 's/"$//' <<<"$SecretAccessKey")
 
 
 export BucketName="$ClusterName"-bucket
@@ -82,8 +84,15 @@ ibmcloud cos create-bucket --bucket "$BucketName" --ibm-service-instance-id $COS
 #Switch kubeconfig context
 ibmcloud oc cluster config --cluster $ClusterName --admin
 
+oc create secret generic image-registry-private-configuration-user --from-literal=REGISTRY_STORAGE_S3_ACCESSKEY="$AccessKeyId" --from-literal=REGISTRY_STORAGE_S3_SECRETKEY="$SecretAccessKey" --namespace openshift-image-registry
+
+#TODO: I want to use encrypt, keyID to integarte with IBM KMS and encrypt images .. WIP
+
+oc patch configs.imageregistry.operator.openshift.io/cluster --type=merge -p '{"spec":{"storage":{"pvc":null,"s3":{"bucket":"'$BucketName'","region":"eu-de","regionEndpoint":"s3.direct.eu-de.cloud-object-storage.appdomain.cloud"}}}}'
+
 #ibmcloud ks kms
 
 #aws --endpoint-url=https://s3.ams.eu.cloud-object-storage.appdomain.cloud s3 ls
+
 
 exit;
--- ibm-cloud/01_delete_cluster.sh
+++ ibm-cloud/01_delete_cluster.sh
@@ -2,14 +2,28 @@ export DataCenterZone=ams03
 export ClusterName="myopenshift"
 
 
-ibmcloud login -a https://api.eu-de.bluemix.net -r eu-de -u rahmed@redhat.com -p xxxxx -c 63cf37b8c3bb448cbf9b7507cc8ca57d -g benelux
+ibmcloud login -a https://api.eu-de.bluemix.net -r eu-de -u rahmed@redhat.com -p xxxxxx -c 63cf37b8c3bb448cbf9b7507cc8ca57d -g benelux
+
+export COSServiceId=$(ibmcloud resource service-instance "$ClusterName"-cos --output json | jq '.[]'|  jq .'crn')
+COSServiceId=$(sed -e 's/^"//' -e 's/"$//' <<<"$COSServiceId")
+
+ibmcloud cos config crn --crn $COSServiceId --force
 
 ibmcloud cos delete-bucket --bucket "$ClusterName"-bucket --force
 
-ibmcloud resource service-key-delete "$ClusterName"-creds -f
+if [ $? -eq 0 ]
+then
+  echo "Bucket Deleted Successfully, deleteing service"
+  ibmcloud resource service-key-delete "$ClusterName"-creds -f
+  ibmcloud resource service-instance-delete "$ClusterName"-cos -f
 
-ibmcloud resource service-instance-delete "$ClusterName"-cos -f
+else
+  echo "Failed to delete Bucket, please try manually"
+fi
 
-ibmcloud oc cluster rm --cluster $ClusterName -f
+#ibmcloud oc cluster rm --cluster $ClusterName -f
+
+
+
 
 exit;
